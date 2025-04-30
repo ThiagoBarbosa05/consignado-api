b73d3e895945d9416ed1d87d85aaf2f9fb6fd5d6
thiago <thiagon16@hotmail.com>
2025-04-30 00:26:58 -0300

    feat: edit and delete user

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -6,6 +6,9 @@ import { authenticate } from "../middleware.ts/authenticate";
 import { checkPermission } from "../middleware.ts/check-permissions";
 import { listRolesController } from "../controllers/list-roles";
 import { listUsersController } from "../controllers/list-users";
+import { getUserController } from "../controllers/get-user";
+import { updateUserController } from "../controllers/update-user";
+import { deleteUserController } from "../controllers/delete-user";
 
 export const userRoute = Router()
 
@@ -13,3 +16,6 @@ userRoute.post("/authenticate", authenticateUserController)
 userRoute.post("/users", authenticate, checkPermission("create:user"),createUserController)
 userRoute.get("/users/roles", authenticate, checkPermission("read:roles"), listRolesController)
 userRoute.get("/users", authenticate, checkPermission("read:users"), listUsersController)
+userRoute.get("/users/:userId", authenticate, checkPermission("read:users"), getUserController)
+userRoute.put("/users/:id", authenticate, checkPermission("update:users"), updateUserController)
+userRoute.delete("/users/:id", authenticate, checkPermission("delete:users"), deleteUserController)
