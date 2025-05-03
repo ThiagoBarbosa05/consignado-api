d84b2e59987fd5523fdcec53c863e14ed109bb3e
ThiagoBarbosa05 <thiagon16@hotmail.com>
2025-05-03 00:44:50 -0300

    feat: consigned details and list

--- src/routes/consigned-route.ts
+++ src/routes/consigned-route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { authenticate } from "../middleware.ts/authenticate";
 import { checkPermission } from "../middleware.ts/check-permissions";
 import { createConsignedController } from "../controllers/create-consigned";
+import { listConsignedController } from "../controllers/list-consigned";
+import { getConsignedController } from "../controllers/get-consigned";
 
 export const consignedRoute = Router();
 
@@ -10,4 +12,18 @@ consignedRoute.post(
   authenticate,
   checkPermission("create:wine"),
   createConsignedController
+);
+
+consignedRoute.get(
+  "/consigned",
+  authenticate,
+  checkPermission("read:wine"),
+  listConsignedController
+);
+
+consignedRoute.get(
+  "/consigned/:id",
+  authenticate,
+  checkPermission("read:wine"),
+  getConsignedController
 );
